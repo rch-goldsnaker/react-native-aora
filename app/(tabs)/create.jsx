4fecde03fbaeccb62a410debbc1784b7d4654312
Roger Chung <84298258+rch-goldsnaker@users.noreply.github.com>
2024-04-16 15:37:22 -0500

    firts commit

---
+++ app/(tabs)/create.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+
+function Create() {
+  return (
+    <View>
+      <Text>Create</Text>
+    </View>
+  )
+}
+
+export default Create
